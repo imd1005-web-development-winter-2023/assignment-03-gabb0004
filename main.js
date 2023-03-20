16548a08d14fac9f8d01a0acb2621354a278a094
gabb0004 <43814756+gabb0004@users.noreply.github.com>
2023-03-20 10:42:30 -0400

    major upload

--- main.js
+++ main.js
@@ -1,44 +1,24 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please enter a task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+      <div class="task">
+      <input type="checkbox">
+      <span id="taskname">
+      ${document.querySelector("#newtask input").value}
+      </span>
+      <button class="delete">
+      <i class="far fa-trash-all"></i>
+      </button>
+      </div>
+    `;
 
-//
-// Variables
-//
-
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// Variables
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
   }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
-}
-
-//
-// Inits & Event Listeners
-//
-
-inititialise();
+};
